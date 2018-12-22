6ea2ca0a8804b8b798ca17a5c6a488267df1ad2d
Jiadong.Yu <jiadong@pandu.ie>
2018-12-22 10:47:12 +0800

    global register Slider component

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 
 import App from './App.vue'
+import Slider from './components/Slider.vue'
 
 import './styles/index.scss'
 import 'swiper/dist/css/swiper.min.css'
+
+Vue.component('Slider', Slider)
 
 Vue.config.productionTip = false
 
