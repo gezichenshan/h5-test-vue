27e15a6015bf4a0941da6be491f91906af6263aa
Jiadong.Yu <jiadong@pandu.ie>
2018-12-24 12:33:37 +0800

    add toFind button

--- src/components/Page2.vue
+++ src/components/Page2.vue
@@ -2,16 +2,33 @@
   <slider class="background">
     <!-- <img src="../assets/images/1.jpg" class="slider-content"> -->
     <!-- <div style="color:#fff;position:absolute;left:0;top:0;font-size:16px;">12312312312</div> -->
+    <div class="search-wrap" @click="toFind()">
+      <Button>去探索</Button>
+    </div>
   </slider>
 </template>
 <script>
-export default {}
+import Button from './Button.vue'
+export default {
+  components: { Button },
+  methods: {
+    toFind() {
+      window.location.href =
+        'https://a.app.qq.com/o/simple.jsp?pkgname=com.cpih.zulin&fromcase=40002'
+    },
+  },
+}
 </script>
 <style lang="scss" scoped>
 .background {
   background-image: url('../assets/images/2.jpg');
   background-repeat: no-repeat;
   background-size: 100%;
+}
+.search-wrap {
+  position: absolute;
+  top: 30rem;
+  left: 38%;
 }
 </style>
 
